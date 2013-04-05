470350030e6ba4802e0ce65662bfe57211aa28ca
Alan Schwarz <alan@atomsign.net>
2013-04-05 16:27:35 -0700

    Secret token is hidden on production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PoetrySite::Application.config.secret_token = '73cfd85c869bdf56671c567b0608f7b4c8d6f6a48a049040b87cbe456b678ea9420a5af1213f11838cd8e44b15a5cc82bba4d974e6cdde9384264986ccd1b96a'
+unless Rails.env.production?
+  PoetrySite::Application.config.secret_token = '73cfd85c869bdf56671c567b0608f7b4c8d6f6a48a049040b87cbe456b678ea9420a5af1213f11838cd8e44b15a5cc82bba4d974e6cdde9384264986ccd1b96a'
+else
+  PoetrySite::Application.config.secret_token = ENV['HONK_HONK']
+end
