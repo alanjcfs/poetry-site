a6a1a5843d9d2a2fb38c16950e319d1318a5b20b
Alan Schwarz <alan@atomsign.net>
2013-07-05 20:57:36 -0700

    No Longer need initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,6 @@ module PoetrySite
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
-    config.logger = Logger.new(STDOUT)
+    # config.logger = Logger.new(STDOUT)
   end
 end
