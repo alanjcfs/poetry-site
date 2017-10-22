24e8409bd492b3ea5c583c729776dec5dc368953
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2017-10-22 14:18:10 -0700

    Migrate, specify rails version, and fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  namespace :admin do
+    resources :poems, except: :destroy
+  end
+
+  resources :poems, only: [:index, :show] do
+    get 'random', on: :collection
+  end
+
+  root to: 'poems#index'
 end
