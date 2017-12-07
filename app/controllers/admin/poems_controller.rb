843b34890a0e123bb5b637bbcbbbd4b2c2ef2648
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2017-12-06 23:54:25 -0800

    Allow admin editing

--- app/controllers/admin/poems_controller.rb
+++ app/controllers/admin/poems_controller.rb
@@ -1,29 +1,35 @@
 class Admin::PoemsController < Admin::ApplicationController
-  respond_to :html, :json
+  def new
+    @poem = Poem.new
+  end
+
+  def create
+    @poem = Poem.new(poem_params)
+
+    if @poem.save
+      redirect_to admin_poem_path(@poem)
+    else
+      render 'new'
+    end
+  end
 
   def index
     @poems = Poem.all
-
-    respond_with @poems
   end
 
   def show
     @poem = Poem.find params[:id]
-
-    respond_with @poem
   end
 
   def edit
     @poem = Poem.find params[:id]
-
-    respond_with @poem
   end
 
   def update
     @poem = Poem.find params[:id]
     @poem.update_attributes(poem_params)
 
-    respond_with @poem, location: admin_poem_url(@poem)
+    redirect_to admin_poem_url(@poem)
   end
 
   protected
